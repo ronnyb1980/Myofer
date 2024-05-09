bc9b0945cbef01590842872869d038b8f2d41b53
ronnyb1980 <bar.ronny@gmail.com>
2024-05-09 10:46:33 +0300

    1st commit

---
+++ AllCategories.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+# Setup Chrome WebDriver
+service = ChromeService(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+# Navigate to the webpage
+driver.get("https://myofer.co.il/malls/ramat-aviv/shopping-online/cat/All-Categories/3000")
+
+# Function to perform click action
+def click_button():
+    try:
+        # Locate the button by XPath and click it
+        button = driver.find_element(By.XPATH, '//*[@id="main-layer"]/div/main/div/div/div/div/div[4]/div[2]/div[3]/a[219]/a/button/span')
+        button.click()
+        print("Button clicked!")
+    except NoSuchElementException:
+        print("Button not found.")
+
+# Loop to click the button every 10 seconds
+try:
+    while True:
+        click_button()
+        time.sleep(2)  # Delay for 10 seconds before next click
+except KeyboardInterrupt:
+    print("Script interrupted by user.")
+
+# Clean up: close the browser window
+driver.quit()
